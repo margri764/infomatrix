99fbf0615f6a53c57a18385ed1a07f335c89f9e0
margri764 <62308899+margri764@users.noreply.github.com>
2023-06-14 22:08:49 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/pages/home/home/home.component';
+import { Page404Component } from './shared/pages/page-404/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "", redirectTo: "home", pathMatch: 'full'
+  },
+  {
+    path: '404',
+    component: Page404Component
+  },
+  {
+    path: '**',
+    // component: ErrorPageComponent
+    redirectTo: '404'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/pages/navbar/navbar/navbar.component';
+import { FooterComponent } from './shared/pages/footer/footer/footer.component';
+import { HomeComponent } from './shared/pages/home/home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Page404Component } from './shared/pages/page-404/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
